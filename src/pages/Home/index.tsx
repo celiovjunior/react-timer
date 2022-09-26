b1ba99aaba5e2f024cd819494d2d1957b5296f43
celiovjunior <celio_cjr@hotmail.com>
2022-09-26 11:41:02 -0300

    change the activeCycle and using it as a params to set the time with useEffect. Timer is running, but with bugs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { differenceInSeconds } from 'date-fns'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -29,6 +31,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -45,11 +48,24 @@ export function Home() {
     },
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const newCycle: Cycle = {
       id: String(new Date().getTime()),
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...cycles, newCycle])
@@ -57,8 +73,6 @@ export function Home() {
 
     reset()
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -85,10 +99,10 @@ export function Home() {
           />
 
           <datalist id="task-suggestion">
-            <option value="fadsf" />
-            <option value="fadf" />
-            <option value="hfghdf" />
-            <option value="trtwtewt" />
+            <option value="Projeto mobile" />
+            <option value="Terminar pagina" />
+            <option value="Estudar pra prova de poo" />
+            <option value="Finalizar o portfolio" />
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
