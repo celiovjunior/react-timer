de9a6a62c6a97999870c682257c59f8bf4bb02b1
celiovjunior <celio_cjr@hotmail.com>
2022-10-10 01:34:21 -0300

    init to create a reduce to create a new cycle

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
@@ -32,7 +33,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{formatDistanceToNow(cycle.startDate)}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishDate && (
                       <Status statusColor="green">Concluído</Status>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -41,6 +41,7 @@ export function Home() {
   const { handleSubmit, watch, reset } = newCycleForm
 
   function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(createNewCycle)
     createNewCycle(data)
     reset()
   }
